348f40183c4b781b8d6fd0872fc3016fb608513b
Thomas Gassmann <thomas.gassmann@hotmail.com>
2021-10-02 16:16:39 +0200

    More test cases for karatsuba algorithm
    Signed-off-by: Thomas Gassmann <thomas.gassmann@hotmail.com>

--- algorithms/src/com/thomasgassmann/KaratsubaTest.java
+++ algorithms/src/com/thomasgassmann/KaratsubaTest.java
@@ -16,6 +16,9 @@ public class KaratsubaTest {
 
     @Test
     public void MultiDigit() {
-        assertEquals(31397584145L, Karatsuba.multiply(2335261, 13445));
+        assertEquals(112111, Karatsuba.multiply(1, 112111));
+        assertEquals(1440, Karatsuba.multiply(12, 120));
+        assertEquals(16134288, Karatsuba.multiply(12, 1344524));
+        assertEquals(15241383936L, Karatsuba.multiply(123456, 123456));
     }
 }
