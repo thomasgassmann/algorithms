f19c5792264c9d3e07f25f5b7f0948b62fdd05db
Thomas Gassmann <thomas.gassmann@hotmail.com>
2021-10-06 22:06:48 +0200

    Add maximum subarray with prefix sum
    Signed-off-by: Thomas Gassmann <thomas.gassmann@hotmail.com>

--- algorithms/src/com/thomasgassmann/MaximumSubarray.java
+++ algorithms/src/com/thomasgassmann/MaximumSubarray.java
@@ -1,6 +1,23 @@
 package com.thomasgassmann;
 
 public class MaximumSubarray {
+    public static int PrefixSums(int[] arr) {
+        int[] prefixSums = new int[arr.length + 1];
+        prefixSums[0] = 0;
+        for (int i = 1; i < arr.length + 1; i++)
+            prefixSums[i] = prefixSums[i - 1] + arr[i - 1];
+
+        int max = 0;
+        for (int i = 0; i < arr.length; i++)
+            for (int j = i; j < arr.length; j++) {
+                int currentSum = prefixSums[j + 1] - prefixSums[i];
+                if (currentSum > max)
+                    max = currentSum;
+            }
+
+        return max;
+    }
+
     public static int Naive(int[] arr) {
         int max = 0;
         for (int i = 0; i < arr.length; i++) {
--- algorithms/src/com/thomasgassmann/MaximumSubarrayTest.java
+++ algorithms/src/com/thomasgassmann/MaximumSubarrayTest.java
@@ -10,4 +10,11 @@ public class MaximumSubarrayTest {
                 7, -11, 15, 110, -23, -3, 127, -12, 1
         }), 226);
     }
+
+    @Test
+    public void PrefixSumAlgorithmCalculatesMaximumSubarray() {
+        assertEquals(MaximumSubarray.PrefixSums(new int[] {
+                7, -11, 15, 110, -23, -3, 127, -12, 1
+        }), 226);
+    }
 }
