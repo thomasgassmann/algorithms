1130a0461cf63c9df93a9aa0badff7f90dacf72b
Thomas Gassmann <thomas.gassmann@hotmail.com>
2021-10-10 01:11:48 +0200

    Add bowshot
    Signed-off-by: Thomas Gassmann <thomas.gassmann@hotmail.com>

---
+++ algorithms/src/com/thomasgassmann/BowShot.java
@@ -0,0 +1,36 @@
+package com.thomasgassmann;
+
+public class BowShot {
+    public static double K = 97442343.345d;
+    public static int count = 0;
+
+    //Prints the distance from the correct result
+    public static void checkAnswer(double result) {
+        System.out.println("Found an answer that was " + Math.abs(result - K) + " meters away. You used " + count + " shots");
+    }
+
+    //shoots an arrow and returns if the arrow hits the wall or not
+    public static boolean shoot(double dist) {
+        count++;
+        return dist < K;
+    }
+
+    public static void evaluate() {
+        double upper = 1;
+        while (shoot(upper)) {
+            upper = 2 * upper;
+        }
+        double lower = upper / 2;
+
+        while ( upper - lower > (2d/100)) {
+            double mid = (upper + lower) / 2;
+            if (shoot(mid)) {
+                lower = mid;
+            } else {
+                upper = mid;
+            }
+        }
+
+        checkAnswer((lower + upper) / 2);
+    }
+}
--- algorithms/src/com/thomasgassmann/Main.java
+++ algorithms/src/com/thomasgassmann/Main.java
@@ -3,5 +3,6 @@ package com.thomasgassmann;
 public class Main {
 
     public static void main(String[] args) {
+        BowShot.evaluate();
     }
 }
