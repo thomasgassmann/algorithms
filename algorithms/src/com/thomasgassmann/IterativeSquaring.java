4f5240f006cc78206f65d807c9eea82c37f87c47
Thomas Gassmann <thomas.gassmann@hotmail.com>
2021-10-11 23:26:35 +0200

    Add iterative squaring
    Signed-off-by: Thomas Gassmann <thomas.gassmann@hotmail.com>

---
+++ algorithms/src/com/thomasgassmann/IterativeSquaring.java
@@ -0,0 +1,22 @@
+package com.thomasgassmann;
+
+public class IterativeSquaring {
+    // power should be n = 2^k
+    public static int SquareEvenPower(int a, int n) {
+        if (n == 0)
+            return 1;
+        if (n == 1)
+            return a;
+        return SquareEvenPower(a * a, n / 2);
+    }
+
+    public static int SquarePower(int a, int n) {
+        if (n == 0)
+            return 1;
+        if (n == 1)
+            return a;
+        if (n % 2 == 0)
+            return SquarePower(a * a, n / 2);
+        return a * SquarePower(a * a, (n - 1) / 2);
+    }
+}
---
+++ algorithms/src/com/thomasgassmann/IterativeSquaringTests.java
@@ -0,0 +1,18 @@
+package com.thomasgassmann;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class IterativeSquaringTests {
+    @Test
+    public void TestSquareEvenPowers() {
+        assertEquals(16, IterativeSquaring.SquareEvenPower(2, 4));
+    }
+
+    @Test
+    public void TestPowers() {
+        assertEquals(16, IterativeSquaring.SquarePower(2, 4));
+        assertEquals(32, IterativeSquaring.SquarePower(2, 5));
+        assertEquals(27, IterativeSquaring.SquarePower(3, 3));
+    }
+}
