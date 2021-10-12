9ba268eb61a5755467e05d5fa5376ee770e21924
Thomas Gassmann <thomas.gassmann@hotmail.com>
2021-10-12 22:27:13 +0200

    Add d
    Signed-off-by: Thomas Gassmann <thomas.gassmann@hotmail.com>

--- algpy/count.py
+++ algpy/count.py
@@ -1,3 +1,4 @@
+import math
 import matplotlib.pyplot as plt
 
 def c(n):
@@ -24,16 +25,28 @@ def b(n):
             calls += 1
     return calls
 
+def d(n):
+    calls = 0
+    for j in range(1, n + 1):
+        k = 1
+        l = 0
+        while k <= j:
+            for m in range(l + 1):
+                calls += 1
+            k = 13 * k
+            l += 1
+    return calls
+
 if __name__ == '__main__':
     xaxis = []
     yaxis = []
     yref = []
-    for i in range(1, 10000, 1000):
+    for i in range(1, 1000000, 100000):
         print(i)
         xaxis.append(i)
-        yaxis.append(c(i))
+        yaxis.append(d(i))
 
-        yref.append(5 * i ** 1.5)
+        yref.append(0.7 * i * math.pow(math.log(i, 13), 2))
 
     plt.plot(xaxis, yaxis, label='f')
     plt.plot(xaxis, yref, label='ref')
