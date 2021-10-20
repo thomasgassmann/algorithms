4cd4993fc61f6855827f755dabeade4058ac3db5
Thomas Gassmann <thomas.gassmann@hotmail.com>
2021-10-20 23:28:38 +0200

    Implement quick sort as in lecture
    Signed-off-by: Thomas Gassmann <thomas.gassmann@hotmail.com>

--- algorithms/src/com/thomasgassmann/QuickSort.java
+++ algorithms/src/com/thomasgassmann/QuickSort.java
@@ -1,21 +1,50 @@
 package com.thomasgassmann;
 
 public class QuickSort {
+    public static void QuickSort(int[] a, int l, int r) {
+        if (l >= 0 && r >= 0 && l < r) {
+            int k = Partition(a, l, r);
+            QuickSort(a, l, k);
+            QuickSort(a, k + 1, r);
+        }
+    }
+
     public static int Partition(int[] a, int left, int right) {
+        int pivot = a[Math.floorDiv(left + right, 2)];
         int i = left - 1;
-        int j = right;
-        int pivot = a[right];
-
-        do {
+        int j = right + 1;
+        while (true) {
             while (a[++i] < pivot);
-            while (--j > left && a[j] > pivot);
+            while (a[--j] > pivot);
 
-            if (i < j) {
+            if (i >= j)
+                return j;
+
+            Sorting.Swap(a, i, j);
+        }
+    }
+
+    public static void QuickSortLecture(int[] a, int left, int right) {
+        if (left < right) {
+            int k = PartitionLecture(a, left, right);
+            QuickSort(a, left, k - 1);
+            QuickSort(a, k + 1, right);
+        }
+    }
+
+    public static int PartitionLecture(int[] a, int l, int r) {
+        int i = l;
+        int j = r - 1;
+        int p = a[r];
+        do {
+            while (i < r && a[i] < p)
+                i++;
+            while (j > l && a[j] > p)
+                j--;
+            if (i < j)
                 Sorting.Swap(a, i, j);
-            }
         } while (i < j);
-
-        Sorting.Swap(a, i, right);
+        Sorting.Swap(a, i, r);
         return i;
     }
 }
--- algorithms/src/com/thomasgassmann/QuickSortTests.java
+++ algorithms/src/com/thomasgassmann/QuickSortTests.java
@@ -5,8 +5,24 @@ import org.junit.jupiter.api.Test;
 
 public class QuickSortTests {
     @Test
+    public void CheckQuickSort() {
+        int[] a = new int[] { 4, 3, 7, 10, 11, 15, 23, 16, 2, 9, 1 };
+        int[] r = new int[] { 1, 2, 3, 4, 7, 9, 10, 11, 15, 16, 23 };
+        QuickSort.QuickSort(a, 0, a.length - 1);
+        SortingTests.AssertEqual(a, r);
+    }
+
+    @Test
+    public void CheckQuickSortLecture() {
+        int[] a = new int[] { 4, 3, 7, 10, 11, 15, 23, 16, 2, 9, 1 };
+        int[] r = new int[] { 1, 2, 3, 4, 7, 9, 10, 11, 15, 16, 23 };
+        QuickSort.QuickSortLecture(a, 0, a.length - 1);
+        SortingTests.AssertEqual(a, r);
+    }
+
+    @Test
     public void CheckPartition() {
-        int[] values = new int[] { 56, 3, 100, 6, 2, 84, 8, 4, 74, 8 };
+        int[] values = new int[] { 56, 3, 6, 100, 6, 2, 84, 8, 4, 74, 6, 8 };
         int res = QuickSort.Partition(values, 0, values.length - 1);
         for (int i = 0; i < res; i++) {
             Assertions.assertTrue(values[res] >= values[i]);
