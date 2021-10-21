bdfa0141c6230cfce1292467228ebbfffa574395
Thomas Gassmann <thomas.gassmann@hotmail.com>
2021-10-22 01:18:20 +0200

    Add nice matrix and fibonacci
    Signed-off-by: Thomas Gassmann <thomas.gassmann@hotmail.com>

---
+++ algorithms/src/com/thomasgassmann/NiceMatrix.java
@@ -0,0 +1,21 @@
+package com.thomasgassmann;
+
+public class NiceMatrix {
+    public static Indicies FindInNiceMatrix(int[][] a, int b) {
+        if (a.length != a[0].length) {
+            throw new IllegalArgumentException();
+        }
+
+        int i = a.length - 1;
+        int j = 0;
+
+        while (a[i][j] != b) {
+            if (a[i][j] < b)
+                j++;
+            else
+                i--;
+        }
+
+        return new Indicies(i, j);
+    }
+}
---
+++ algorithms/src/com/thomasgassmann/NiceMatrixTests.java
@@ -0,0 +1,22 @@
+package com.thomasgassmann;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class NiceMatrixTests {
+    @Test
+    public void CheckNiceMatrix() {
+        int[][] nice = new int[][]{
+                { 1, 2, 5, 7 },
+                { 3, 4, 6, 8 },
+                { 4, 5, 7, 9 },
+                { 6, 9, 10, 11 }
+        };
+
+        int b = 8;
+
+        Indicies ind = NiceMatrix.FindInNiceMatrix(nice, b);
+        Assertions.assertEquals(ind.getI(), 1);
+        Assertions.assertEquals(ind.getJ(), 3);
+    }
+}
