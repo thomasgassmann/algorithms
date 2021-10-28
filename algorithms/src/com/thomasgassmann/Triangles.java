7ac5ed9a1b3e11ff88de2125e4361b7be258ddb3
Thomas Gassmann <thomas.gassmann@hotmail.com>
2021-10-29 01:07:12 +0200

    I copied the wrong values
    Signed-off-by: Thomas Gassmann <thomas.gassmann@hotmail.com>

--- algorithms/src/com/thomasgassmann/Triangles.java
+++ algorithms/src/com/thomasgassmann/Triangles.java
@@ -1,63 +1,26 @@
 package com.thomasgassmann;
 
 public class Triangles {
-    private static class SortedValue {
-        private int _a;
-        private int _i;
-
-        public SortedValue(int a, int i) {
-            _a = a;
-            _i = i;
-        }
-
-        public int getIndex() {
-            return _i;
-        }
-
-        public int getValue() {
-            return _a;
-        }
-    }
-
     public static long CountTriangles(int n, int[] A) {
-        SortedValue[] values = new SortedValue[n];
-        for (int i = 0; i < n; i++)
-            values[i] = new SortedValue(A[i], i);
-
-        QuickSort(values, 0, n - 1);
+        QuickSort(A, 0, n - 1);
 
         long triangles = 0;
         for (int i = 0; i < n - 2; i++) {
             int k = i + 2;
-            for (int j = i + 1; j < n - 1; j++) {
-                long currentTriangles = 0;
-                int ival = values[i].getValue();
-                int jval = values[j].getValue();
-                while (k < n) {
-                    int kval = values[k].getValue();
-                    if (ival + jval < kval)
-                        break;
-
-                    if (values[j].getIndex() < values[k].getIndex() ||
-                        values[i].getIndex() < values[k].getIndex()) {
-                        currentTriangles++;
-                    }
-
+            for (int j = i + 1; j < n; j++) {
+                while (k < n && A[i] + A[j] >= A[k]) {
                     k++;
                 }
 
-                triangles += currentTriangles;
+                if (k > j)
+                    triangles += k - j - 1;
             }
         }
 
         return triangles;
     }
 
-    public static boolean IsTriangle(int a, int b, int c) {
-        return (a + b) >= c && (c + a) >= b && (b + c) >= a;
-    }
-
-    public static void QuickSort(SortedValue[] a, int l, int r) {
+    public static void QuickSort(int[] a, int l, int r) {
         if (l < r) {
             int k = Partition(a, l, r);
             QuickSort(a, l, k);
@@ -65,18 +28,18 @@ public class Triangles {
         }
     }
 
-    public static int Partition(SortedValue[] a, int left, int right) {
-        SortedValue pivot = a[Math.floorDiv(left + right, 2)];
+    public static int Partition(int[] a, int left, int right) {
+        int pivot = a[Math.floorDiv(left + right, 2)];
         int i = left - 1;
         int j = right + 1;
         while (true) {
-            while (a[++i].getValue() < pivot.getValue()) ;
-            while (a[--j].getValue() > pivot.getValue()) ;
+            while (a[++i] < pivot) ;
+            while (a[--j] > pivot) ;
 
             if (i >= j)
                 return j;
 
-            SortedValue tmp = a[i];
+            int tmp = a[i];
             a[i] = a[j];
             a[j] = tmp;
         }
