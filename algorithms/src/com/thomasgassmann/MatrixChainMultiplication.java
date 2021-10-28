f538267cb377d482ae9004f4a1a0de7572493cbe
Thomas Gassmann <thomas.gassmann@hotmail.com>
2021-10-28 17:40:39 +0200

    Finish optimal associativity
    Signed-off-by: Thomas Gassmann <thomas.gassmann@hotmail.com>

--- algorithms/src/com/thomasgassmann/MatrixChainMultiplication.java
+++ algorithms/src/com/thomasgassmann/MatrixChainMultiplication.java
@@ -1,12 +1,37 @@
 package com.thomasgassmann;
 
+import java.util.ArrayList;
+
 public class MatrixChainMultiplication {
-    public static int MostEfficientMatrixAssociativity(int[][][] matrices) {
+    public static class Associativity {
+        private int _from;
+        private int _to;
+
+        public Associativity(int from, int to) {
+            _from = from;
+            _to = to;
+        }
+
+        public int from() {
+            return _from;
+        }
+
+        public int to() {
+            return _to;
+        }
+    }
+
+    public ArrayList<Associativity> MostEfficientMatrixAssociativity(int[][][] matrices) {
         int[][] dp = new int[matrices.length][matrices.length];
+
+        var assoc = new ArrayList<Associativity>();
 
         // for each col
         for (int p = 0; p < dp.length; p++) {
             // move along diagonal to the lower right
+            int minP = Integer.MAX_VALUE;
+            int minQ = Integer.MAX_VALUE;
+            int diagonalMin = Integer.MAX_VALUE;
             for (int q = 0; q < dp.length - p; q++) {
                 int currentQ = p + q;
                 int currentP = q;
@@ -18,13 +43,25 @@ public class MatrixChainMultiplication {
                     int cost = matrices[currentP].length *
                                matrices[i][0].length *
                                matrices[currentQ][0].length;
-                    min = Math.min(dp[currentP][i] + dp[i + 1][currentQ] + cost, min);
+                    int total = dp[currentP][i] + dp[i + 1][currentQ] + cost;
+                    if (total < min) {
+                        min = total;
+                    }
                 }
 
                 dp[currentP][currentQ] = min == Integer.MAX_VALUE ? 0 : min;
+                if (min < diagonalMin) {
+                    diagonalMin = min;
+                    minP = currentP;
+                    minQ = currentQ;
+                }
+            }
+
+            if (p != 0) {
+                assoc.add(new Associativity(minP, minQ));
             }
         }
 
-        return dp[0][dp[0].length - 1];
+        return assoc;
     }
 }
--- algorithms/src/com/thomasgassmann/MatrixChainMultiplicationTests.java
+++ algorithms/src/com/thomasgassmann/MatrixChainMultiplicationTests.java
@@ -3,22 +3,37 @@ package com.thomasgassmann;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 public class MatrixChainMultiplicationTests {
     @Test
     public void CheckOptimalMatrixChosen() {
         var matrices = new int[][][]{
                 {
-                        {1}, {2}, {3}
+                        {1}, {-1}, {2}, {-2}, {3}, {-3}
                 },
                 {
-                        {3, 2, 1}
+                        {3, -3, 2, -2, 1, -1}
                 },
                 {
-                        {4}, {5}, {6}
+                        {4}, {-4}, {5}, {-5}, {6}, {-6}
+                },
+                {
+                        {1, 2, 3, 4, 5}
                 }
         };
 
-        int cost = MatrixChainMultiplication.MostEfficientMatrixAssociativity(matrices);
-        Assertions.assertEquals(cost, 6);
+        var target = new MatrixChainMultiplication.Associativity[3];
+        target[0] = new MatrixChainMultiplication.Associativity(0, 3);
+        target[1] = new MatrixChainMultiplication.Associativity(1, 2);
+        target[2] = new MatrixChainMultiplication.Associativity(1, 3);
+
+        ArrayList<MatrixChainMultiplication.Associativity> assocs =
+                new MatrixChainMultiplication()
+                        .MostEfficientMatrixAssociativity(matrices);
+        for (MatrixChainMultiplication.Associativity targetVal : target) {
+            Assertions.assertTrue(assocs.stream().anyMatch(p -> p.from() == targetVal.from() &&
+                                          p.to() == targetVal.to()));
+        }
     }
 }
