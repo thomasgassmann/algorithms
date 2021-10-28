5fcf8a9d4915f7e245f62616b06d491a83be8df4
Thomas Gassmann <thomas.gassmann@hotmail.com>
2021-10-28 13:44:45 +0200

    Calculate minimal cost of matrix multiplication
    Signed-off-by: Thomas Gassmann <thomas.gassmann@hotmail.com>

--- algorithms/src/com/thomasgassmann/MatrixChainMultiplication.java
+++ algorithms/src/com/thomasgassmann/MatrixChainMultiplication.java
@@ -1,26 +1,30 @@
 package com.thomasgassmann;
 
 public class MatrixChainMultiplication {
-    public void MostEfficientMatrixAssociativity(int[][][] matrices) {
+    public static int MostEfficientMatrixAssociativity(int[][][] matrices) {
         int[][] dp = new int[matrices.length][matrices.length];
-        for (int i = 0; i < dp.length; i++)
-            dp[i][i] = 0;
 
+        // for each col
         for (int p = 0; p < dp.length; p++) {
-            for (int q = 0; q < dp.length; q++) {
+            // move along diagonal to the lower right
+            for (int q = 0; q < dp.length - p; q++) {
+                int currentQ = p + q;
+                int currentP = q;
+
                 int min = Integer.MAX_VALUE;
-                for (int i = p; i < q; i++) {
+                for (int i = currentP; i < currentQ; i++) {
                     // cost of multiplying
                     // (matrices[p] * ... * matrices[i]) * (matrices[i + 1] * matrices[q])
-                    int cost = matrices[p - 1].length *
+                    int cost = matrices[currentP].length *
                                matrices[i][0].length *
-                               matrices[q][0].length;
-                    min = Math.min(dp[p][i] + dp[i + 1][q] + cost, min);
+                               matrices[currentQ][0].length;
+                    min = Math.min(dp[currentP][i] + dp[i + 1][currentQ] + cost, min);
                 }
 
-                dp[p][q] = min;
+                dp[currentP][currentQ] = min == Integer.MAX_VALUE ? 0 : min;
             }
         }
 
+        return dp[0][dp[0].length - 1];
     }
 }
--- algorithms/src/com/thomasgassmann/MatrixChainMultiplicationTests.java
+++ algorithms/src/com/thomasgassmann/MatrixChainMultiplicationTests.java
@@ -1,4 +1,24 @@
 package com.thomasgassmann;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MatrixChainMultiplicationTests {
+    @Test
+    public void CheckOptimalMatrixChosen() {
+        var matrices = new int[][][]{
+                {
+                        {1}, {2}, {3}
+                },
+                {
+                        {3, 2, 1}
+                },
+                {
+                        {4}, {5}, {6}
+                }
+        };
+
+        int cost = MatrixChainMultiplication.MostEfficientMatrixAssociativity(matrices);
+        Assertions.assertEquals(cost, 6);
+    }
 }
