43fd65d03858bc20ec99e2e3d6fa2cb33eae17cd
Thomas Gassmann <thomas.gassmann@hotmail.com>
2021-11-09 12:42:21 +0100

    Add LongestSnake boilerplate
    Signed-off-by: Thomas Gassmann <thomas.gassmann@hotmail.com>

---
+++ algorithms/src/com/thomasgassmann/LongestSnake.java
@@ -0,0 +1,10 @@
+package com.thomasgassmann;
+
+import java.util.HashMap;
+
+public class LongestSnake {
+    public static int LongestSnake(int[] values, HashMap<Integer, int[]> neighbors) {
+        int[][] dp = new int[values.length][values.length];
+        return 0;
+    }
+}
