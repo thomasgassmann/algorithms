e8dcb0975b17f4e015a1dd8afb2c405e1f828d6c
Thomas Gassmann <thomas.gassmann@hotmail.com>
2021-11-14 14:00:12 +0100

    Add extended euclidian algorithm
    Signed-off-by: Thomas Gassmann <thomas.gassmann@hotmail.com>

--- algorithms/src/com/thomasgassmann/EuclidianGCD.java
+++ algorithms/src/com/thomasgassmann/EuclidianGCD.java
@@ -17,6 +17,45 @@ public class EuclidianGCD {
         return n;
     }
 
+    public static class ExtendedGCD {
+        public ExtendedGCD(int u, int v, int gcd) {
+            this.u = u;
+            this.v = v;
+            this.gcd = gcd;
+        }
+
+        public int u;
+        public int v;
+        public int gcd;
+    }
+
+    public static ExtendedGCD ExtendedGCD(int a, int b) {
+        int old_r = a;
+        int r = b;
+        int old_s = 1;
+        int s = 0;
+        int old_t = 0;
+        int t = 1;
+
+        while (r != 0) {
+            int quotient = old_r / r;
+
+            int tmpr = r;
+            r = old_r - quotient * r;
+            old_r = tmpr;
+
+            int tmps = s;
+            s = old_s - quotient * s;
+            old_s = tmps;
+
+            int tmpt = t;
+            t = old_t - quotient * t;
+            old_t = tmpt;
+        }
+
+        return new ExtendedGCD(old_s, old_t, old_r);
+    }
+
     public static long GCD(long n, long m) {
         if (m > n) {
             long tmp = m;
--- algorithms/src/com/thomasgassmann/EuclidianGCDTest.java
+++ algorithms/src/com/thomasgassmann/EuclidianGCDTest.java
@@ -1,9 +1,18 @@
 package com.thomasgassmann;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EuclidianGCDTest {
+    @Test
+    public void checkExtendedGCD() {
+        var res = EuclidianGCD.ExtendedGCD(142, 553);
+        Assertions.assertEquals(res.gcd, 1);
+        Assertions.assertEquals(res.u, 74);
+        Assertions.assertEquals(res.v, -19);
+    }
+
     @Test
     public void TestGCDCalculation() {
         assertEquals(EuclidianGCD.GCD(15, 5), 5);
