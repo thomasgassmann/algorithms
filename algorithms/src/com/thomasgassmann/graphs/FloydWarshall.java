6e680ab57d8c34d155d57f9e3f3f5c6c1a13ad09
Thomas Gassmann <thomas.gassmann@hotmail.com>
2021-12-16 11:01:39 +0100

    Improve space complexity of Floyd Warshall
    Signed-off-by: Thomas Gassmann <thomas.gassmann@hotmail.com>

--- algorithms/src/com/thomasgassmann/graphs/FloydWarshall.java
+++ algorithms/src/com/thomasgassmann/graphs/FloydWarshall.java
@@ -3,13 +3,13 @@ package com.thomasgassmann.graphs;
 public class FloydWarshall {
     public static Integer[][] FloydWarshall(GraphWithWeights<Integer> graph) {
         var n = graph.getVertices().size();
-        Integer[][][] d = new Integer[n][n][n];
+        Integer[][] d = new Integer[n][n];
         for (int i = 0; i < n; i++)
-            d[0][i][i] = 0;
+            d[i][i] = 0;
 
         for (int u = 0; u < n; u++) {
             for (int v = 0; v < n; v++) {
-                d[0][u][v] = graph.hasEdge(u, v)
+                d[u][v] = graph.hasEdge(u, v)
                         ? graph.getWeight(u, v)
                         : null;
             }
@@ -25,11 +25,10 @@ public class FloydWarshall {
         for (int i = 1; i < n; i++) {
             for (int u = 0; u < n; u++) {
                 for (int v = 0; v < n; v++) {
-                    d[i][u][v] = d[i - 1][u][v];
-                    if (d[i - 1][u][i] != null && d[i - 1][i][v] != null) {
-                        var combined = d[i - 1][u][i] + d[i - 1][i][v];
-                        if (d[i][u][v] == null || d[i][u][v] > combined) {
-                            d[i][u][v] = combined;
+                    if (d[u][i] != null && d[i][v] != null) {
+                        var combined = d[u][i] + d[i][v];
+                        if (d[u][v] == null || d[u][v] > combined) {
+                            d[u][v] = combined;
                             succ[u][v] = succ[u][i];
                         }
                     }
